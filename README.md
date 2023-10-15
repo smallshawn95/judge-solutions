06b9649a84ca98b448f21cd5eae11a1215617134
smallshawn95 <smallshawn95@gmail.com>
2023-10-15 22:43:43 +0800

    2023-10-15 v1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ZeroJudge-Code
+# ZeroJudge Code
+SmallShawn95 的 ZeroJudge 刷題紀錄，<br>
+點此可以查看我的 [ZeroJudge 個人刷題頁面](https://zerojudge.tw/UserStatistic?id=172927)。
---
+++ a003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int day,month,ans;
+    cin >> month >> day;
+    ans = (month * 2 + day) % 3;
+    if (ans == 0)
+        cout << "´¶³q";
+    else if (ans == 1)
+        cout << "¦N";
+    else if (ans == 2)
+        cout << "¤j¦N";
+    return 0;
+}
