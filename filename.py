67c93a1cab337de1b24627253a5be264e33f9a9e
smallshawn95 <smallshawn95@gmail.com>
2024-03-01 10:48:19 +0800

    2024-03-01 filename.py

---
+++ filename.py
@@ -0,0 +1,13 @@
+# pip install pyperclip
+
+import re, pyperclip
+
+def get_underscore_title(title: str):
+    underscore_title = re.sub(r'[^a-zA-Z]+', '_', title)
+    return underscore_title
+
+if __name__ == "__main__":
+    title = input("Title: ")
+    underscore_title = get_underscore_title(title)
+    print(underscore_title)
+    pyperclip.copy(underscore_title)
